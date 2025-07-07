2d800b7bbeb13b3cf2828f17d377a3b1465718f4
hotty0914 <hkkomamoko@icloud.com>
2025-07-07 20:59:28 +0900

    Update __init__.py

--- ch10_3_dictator/__init__.py
+++ ch10_3_dictator/__init__.py
@@ -8,38 +8,38 @@ class C(BaseConstants):
     NAME_IN_URL = "ch10_3_dictator"
     PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 1
-    ENDOWMENT = 1000
+    ENDOWMENT = 100
     INSTRUCTIONS_TEMPLATE = "ch10_3_dictator/instructions.html"
     ADD_TIME = 180
 
     CHOICE_LIST_SENTE = [
-        [0, "あなた0ポイント、相手1000ポイント"],
-        [100, "あなた100ポイント、相手900ポイント"],
-        [200, "あなた200ポイント、相手800ポイント"],
-        [300, "あなた300ポイント、相手700ポイント"],
-        [400, "あなた400ポイント、相手600ポイント"],
-        [500, "あなた500ポイント、相手500ポイント"],
-        [600, "あなた600ポイント、相手400ポイント"],
-        [700, "あなた700ポイント、相手300ポイント"],
-        [800, "あなた800ポイント、相手200ポイント"],
-        [900, "あなた900ポイント、相手100ポイント"],
-        [1000, "あなた1000ポイント、相手0ポイント"],
+        [0, "あなた0ポイント、相手100ポイント"],
+        [10, "あなた10ポイント、相手90ポイント"],
+        [20, "あなた20ポイント、相手80ポイント"],
+        [30, "あなた30ポイント、相手70ポイント"],
+        [40, "あなた40ポイント、相手60ポイント"],
+        [50, "あなた50ポイント、相手50ポイント"],
+        [60, "あなた60ポイント、相手40ポイント"],
+        [70, "あなた70ポイント、相手30ポイント"],
+        [80, "あなた80ポイント、相手20ポイント"],
+        [90, "あなた90ポイント、相手10ポイント"],
+        [100, "あなた100ポイント、相手0ポイント"],
     ]
 
 
 class Subsession(BaseSubsession):
     num_participants_p1 = models.IntegerField(initial=0)
     num_participants_p2 = models.IntegerField(initial=0)
-    num_1000 = models.IntegerField(initial=0)
-    num_900 = models.IntegerField(initial=0)
-    num_800 = models.IntegerField(initial=0)
-    num_700 = models.IntegerField(initial=0)
-    num_600 = models.IntegerField(initial=0)
-    num_500 = models.IntegerField(initial=0)
-    num_400 = models.IntegerField(initial=0)
-    num_300 = models.IntegerField(initial=0)
-    num_200 = models.IntegerField(initial=0)
     num_100 = models.IntegerField(initial=0)
+    num_90 = models.IntegerField(initial=0)
+    num_80 = models.IntegerField(initial=0)
+    num_70 = models.IntegerField(initial=0)
+    num_60 = models.IntegerField(initial=0)
+    num_50 = models.IntegerField(initial=0)
+    num_40 = models.IntegerField(initial=0)
+    num_30 = models.IntegerField(initial=0)
+    num_20 = models.IntegerField(initial=0)
+    num_10 = models.IntegerField(initial=0)
     num_0 = models.IntegerField(initial=0)
     num_accept = models.IntegerField(initial=0)
     num_reject = models.IntegerField(initial=0)
@@ -76,26 +76,26 @@ def set_P1(player: Player):
         sub.num_participants_p1 += 1
         print("++++++++++++++++", group.p1_decision)
         s = group.p1_decision
-        if s == "1000":
-            sub.num_1000 += 1
-        elif s == "900":
-            sub.num_900 += 1
-        elif s == "800":
-            sub.num_800 += 1
-        elif s == "700":
-            sub.num_700 += 1
-        elif s == "600":
-            sub.num_600 += 1
-        elif s == "500":
-            sub.num_500 += 1
-        elif s == "400":
-            sub.num_400 += 1
-        elif s == "300":
-            sub.num_300 += 1
-        elif s == "200":
-            sub.num_200 += 1
-        elif s == "100":
+        if s == "100":
             sub.num_100 += 1
+        elif s == "90":
+            sub.num_90 += 1
+        elif s == "80":
+            sub.num_80 += 1
+        elif s == "70":
+            sub.num_70 += 1
+        elif s == "60":
+            sub.num_60 += 1
+        elif s == "50":
+            sub.num_50 += 1
+        elif s == "40":
+            sub.num_40 += 1
+        elif s == "30":
+            sub.num_30 += 1
+        elif s == "20":
+            sub.num_20 += 1
+        elif s == "10":
+            sub.num_10 += 1
         elif s == "0":
             sub.num_0 += 1
         else:
@@ -140,53 +140,53 @@ def graph(subsession: Subsession):
         graph_list.append(tmp)
     else:
         graph_list.append(0)
+    if sub.num_10 > 0:
+        tmp = round((sub.num_10 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_20 > 0:
+        tmp = round((sub.num_20 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_30 > 0:
+        tmp = round((sub.num_30 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_40 > 0:
+        tmp = round((sub.num_40 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_50 > 0:
+        tmp = round((sub.num_50 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_60 > 0:
+        tmp = round((sub.num_60 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_70 > 0:
+        tmp = round((sub.num_70 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_80 > 0:
+        tmp = round((sub.num_80 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
+    if sub.num_90 > 0:
+        tmp = round((sub.num_90 / sub.num_participants) * 100, 2)
+        graph_list.append(tmp)
+    else:
+        graph_list.append(0)
     if sub.num_100 > 0:
         tmp = round((sub.num_100 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_200 > 0:
-        tmp = round((sub.num_200 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_300 > 0:
-        tmp = round((sub.num_300 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_400 > 0:
-        tmp = round((sub.num_400 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_500 > 0:
-        tmp = round((sub.num_500 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_600 > 0:
-        tmp = round((sub.num_600 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_700 > 0:
-        tmp = round((sub.num_700 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_800 > 0:
-        tmp = round((sub.num_800 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_900 > 0:
-        tmp = round((sub.num_900 / sub.num_participants) * 100, 2)
-        graph_list.append(tmp)
-    else:
-        graph_list.append(0)
-    if sub.num_1000 > 0:
-        tmp = round((sub.num_1000 / sub.num_participants) * 100, 2)
         graph_list.append(tmp)
     else:
         graph_list.append(0)
